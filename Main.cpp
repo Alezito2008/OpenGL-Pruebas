42bf9352df1dd72b153b5449a12d9e78c5ec4f27
Alezito2008 <alejandro.kretzig@gmail.com>
2025-07-24 02:51:22 -0300

    Cuadrado

--- Main.cpp
+++ Main.cpp
@@ -25,7 +25,19 @@ void processInput(GLFWwindow* window) {
 	}
 }
 
+void checkShaderCompileErrors(const unsigned int shader, const char* shaderType) {
+	int success;
+	char infoLog[512];
+
+	glad_glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "Error compilando shader del tipo " << shaderType << ": " << infoLog << std::endl;
+	}
+}
+
 int main() {
+
 	glfwInit();
 
 	// Decirle a OpenGL los datos sobre la versión
@@ -52,54 +64,32 @@ int main() {
 
 	// Especificar el viewport
 	glViewport(0, 0, 800, 800);
+	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
-	};
+	// -- Creación de shaders -- //
 
-	// Crear variable para guardar id del vertex shader
 	unsigned int vertexShader;
-	// Se le asigna el id del shader creado y el tipo
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	// Cargar y compilar el codigo fuente del shader
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
 
-	// Variable para guardar el estado de compilación
-	int success;
-	// String para almacenar información en caso de haber errores
-	char infoLog[512];
-	// Obtener el estado de compilación y guardarlo en success
-	glad_glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		// Si hay errores, guarda el error en infoLog
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "Error al compilar vertex shader: " << infoLog << std::endl;
-	}
+	checkShaderCompileErrors(vertexShader, "vertex");
 
-	// Crear fragment shader y compilar
 	unsigned int fragmentShader;
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 
-	glad_glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "Error al compilar fragment shader: " << infoLog << std::endl;
-	}
+	checkShaderCompileErrors(fragmentShader, "shader");
 
-	// ShaderProgram es una combinación de varios shaders
 	unsigned int shaderProgram;
-	// Se asigna el id del nuevo program a shaderProgram
 	shaderProgram = glCreateProgram();
-	// Le dice que shaders se van a usar en el program
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
-	// Termina el enlace
 	glLinkProgram(shaderProgram);
+
+	int success;
+	char infoLog[512];
 
 	glad_glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 	if (!success) {
@@ -107,64 +97,51 @@ int main() {
 		std::cout << "Error al linkear shaders: " << infoLog << std::endl;
 	}
 
-	// Ya no necesitamos los shaders que creamos así que los eliminamos
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	// VAO es donde se pueden guardar configuraciones como el VBO y los attribs que usa
-	unsigned int VAO;
-	// Le asigna un id al VAO
-	glad_glGenVertexArrays(1, &VAO);
-	// Comienza la escritura
+	float vertices[] = {
+		0.5f, 0.5f, 0.0f, // arriba derecha
+		0.5f, -0.5f, 0.0f, // abajo derecha
+		-0.5f, -0.5f, 0.0f, // abajo izquierda
+		-0.5f, 0.5f, 0.0f // arriba izquierda
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3, // primer triángulo
+		1, 2, 3 // segundo triángulo
+	};
+
+	unsigned int VAO, VBO, EBO;
+
+	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
-	// -- 0. Copiar los vertices a un buffer para que lo use OpenGL -- //
-	// VBO = Vertex Buffer Object (un contenedor de vertices para la memoria en el GPU), VBO contiene el id del buffer
-	unsigned int VBO;
-	// Se genera un identificador de buffer
-	glad_glGenBuffers(1, &VBO);
-	// Se asigna el tipo de buffer (array)
+	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// Copia los datos de los vértices al buffer
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// -- 1. Definir los atributos que recibe el shader -- //
-	// Le dice al GPU que hay un atributo (es como un argumento que se le pasa a los shaders
-	// en la posición 0 (primer arg), que son 3 datos, que es un float, que cada 3 floats es uno de los atributos
-	// y empieza desde el primer espacio de memoria
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// -- 2. Decirle a OpenGL que programa usar -- //
-	// Usar el shaderProgram, cada llamada nueva que use un shader va a usar este
-	glUseProgram(shaderProgram);
-	// Comienza a utilizar el VAO
-	glBindVertexArray(VAO);
-	// Dibujar triángulo
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	 
+	glBindVertexArray(0);
 
 
-	// Loop principal
 	while (!glfwWindowShouldClose(window)) {
-		// Procesar teclas
 		processInput(window);
 
-		// Especificar color de fondo
 		glClearColor(0.0, 0.0, 0.5, 1.0);
-		// Limpiar el buffer de fondo y asignarle el color
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Usar el shaderProgram, cada llamada nueva que use un shader va a usar este
 		glUseProgram(shaderProgram);
-		// Comienza a utilizar el VAO
 		glBindVertexArray(VAO);
-		// Dibujar triángulo
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-		// Procesar eventos
 		glfwPollEvents();
-		// Intercambiar buffer hacia el nuevo
 		glfwSwapBuffers(window);
 	}
 
